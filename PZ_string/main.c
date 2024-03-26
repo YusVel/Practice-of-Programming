85fd4bd181614aa7086fc7b44ec1f2dd642088bc
Yusup Velimetov <yusupvelimetov@mail.ru>
2024-03-26 21:52:09 +0300

    123

--- PZ_string/main.c
+++ PZ_string/main.c
@@ -20,17 +20,47 @@ void memorymove()
 
 int main()
 { 
-	char str1[] = "All that I have is all that you've given me Did you never worry that I'd come to depend on you...!";
-	char str2[] = "Aaeyuioj\n";
-	printf("%s\n",str1);
-	printf("Длина строки: %d\n",strlen(str1));
+	FILE *file = NULL;
 	
-	for(int i = 0;i<strlen(str1);i++)
+	if (fopen_s(&file, "Data.txt", "r")!=0)
 	{
-		memmove(str1+i,str1+(i++),(strlen(str1)-i));
+		printf("The openning is failed");
 	}
-	printf("%s\n",str1);
+	
+	double size = 0;
+	char t = 'r';
+	while (fscanf_s(file, "%c", &t)!=EOF)
+	{
+		size++;
+	}
+	printf("VOLUM = %5.0lf baits\n", size);
+	fseek(file, NULL, SEEK_SET);
 
-	//memorymove();
+	char* str1 =(char*)malloc(size);
+
+	int i = 0;
+	while (fscanf_s(file, "%c", &t) != EOF)
+	{
+		str1[i++] = t;
+	}
+
+	printf("%s\n\n", str1);
+	printf("%d", strlen(str1));
+	
+	char str2[] = " ";
+	printf("%s\n",str1);
+	printf("Length str: %d\n",strlen(str1));
+	
+	for(int i = strcspn(str1, str2);i<strlen(str1)-1;)
+	{
+		memmove(str1+i,str1+i+1,(strlen(str1)-i));
+		i = strcspn(str1, str2); 
+
+	}
+	printf("%s\n", str1);
+	printf("%d", strlen(str1));
+
+	//free(str1);
+	fclose(file);
 	return 0;
 }
